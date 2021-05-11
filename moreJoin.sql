369d1d651085a6a164e33cacb0cc89df5112099f
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-10 22:14:19 -0500

    question 7

--- moreJoin.sql
+++ moreJoin.sql
@@ -42,3 +42,11 @@ FROM actor
 JOIN casting ON actorid = actor.id 
 JOIN movie ON movie.id = casting.movieid
 WHERE title = 'Casablanca';
+
+/* 7. Obtain the cast list for the film 'Alien' */
+
+SELECT name
+FROM actor 
+JOIN casting ON actorid = actor.id 
+JOIN movie ON movie.id = casting.movieid
+WHERE title = 'Alien';
