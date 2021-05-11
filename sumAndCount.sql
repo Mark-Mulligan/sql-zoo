395bbc8b2d5307493e832ec878a1e7844d792481
Mark Mulligan <mark.mulligan.jr1@gmail.com>
2021-05-10 21:56:48 -0500

    question 7

--- sumAndCount.sql
+++ sumAndCount.sql
@@ -31,3 +31,10 @@ WHERE name IN ('Estonia', 'Latvia', 'Lithuania');
 SELECT continent, COUNT(name)
 FROM world
 GROUP BY continent;
+
+/* 7.  For each continent show the continent and number of countries with populations of at least 10 million. */
+
+SELECT continent, COUNT(continent)
+FROM world
+WHERE population > 10000000
+GROUP BY continent;
